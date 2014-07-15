10649b965c2ed22940d3d16db7998fb9a3c6122b
Vyivrain <sashaverhun@rambler.ru>
2014-07-15 21:05:56 +0300

    Third iteration

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,8 +1,19 @@
 class PhotoController < ApplicationController
 	
 	def index
-		@photo = MyPhoto.search( params[:tag], params[:tags], params[:user_id], 
-			params[:coords], params[:check_in] )
+
+		@radioButName ||= { 
+			tags: "Tags search" ,
+			user_id_tag: "User id and tag search" ,
+		    coords_user: "Coordinate and user search",
+		    coords_tag: "Coordinate and tag search", 
+		    check_in_tag: "Check-in tag search",
+		    check_in_user: "Check-in user search",
+		    check_in_tag_user: "Check-in, tag and user search"
+		}
+
+		 searchManage
+
 	end
 
 	def new
@@ -11,4 +22,28 @@ class PhotoController < ApplicationController
 
 	def create
 	end
+
+	def searchManage
+
+		case params[:check_tag]
+		when "tags"
+			@photo = MyPhoto.tagsearch( params[:tag] )
+		when "user_id_tag"
+			@photo = MyPhoto.id_tag_Search( params[:tag] )
+		when "coords_user"
+			@photo = MyPhoto.coord_user_Search( params[:tag], true )
+		when "coords_tag"
+			@photo = MyPhoto.coord_user_Search( params[:tag], false )
+		when "check_in_tag"
+			@photo = MyPhoto.check_in_tag_Search( params[:tag], false )
+		when "check_in_user"
+			@photo = MyPhoto.check_in_tag_Search( params[:tag], true )
+		when "check_in_tag_user"
+			@photo = MyPhoto.check_in_tag_user_id_Search( params[:tag] )
+		end
+
+	end
+
+	
+  	
 end
--- app/models/my_photo.rb
+++ app/models/my_photo.rb
@@ -1,81 +1,52 @@
 class MyPhoto < ActiveRecord::Base
 
-	def self.search( tag, tags, user_id, coords, check_in )
-		
-		if tag 
-			tagArr = tag.split
-			result = Array.new
-			first = true
-
-			tagArr.each {
-				|oneTag|
-
-				if user_id and is_number?( oneTag )
-
-					if first
-						result.concat( idSearch( oneTag ) )
-						first = false
-					else
-						response = idSearch( oneTag )
-						result = result & response 
-					end 
-
-				elsif coords and is_coord?( oneTag )
-
-					if first
-						result.concat( coordSearch( oneTag ) )
-						first = false
-					else
-						response = coordSearch( oneTag )
-						result = result & response
-					end
-				  	 
-				elsif tags and oneTag.length <= 10
-
-					if first
-						result.concat( tagsearch( oneTag ) ) 
-						first = false
-					else
-						response = tagsearch( oneTag )
-						result = result & response
-					end
-					
-				elsif check_in and oneTag.length >= 20
-
-					if first
-						result.concat( checkinSearch( oneTag ) )
-						first = false
-					else
-						response = checkinSearch( oneTag )
-						result = result & response
-					end
-					
-				else
-					[]
-				end
-			}
-
-			result.each {
-				|element|
-
-			}
-
-			result
-		else
-			[]
-		end
-
-	end
-
+	# Multiple tag example "cat kitten"
+	# Max tags 2
 	def self.tagsearch( tag )
 		
-		 JSON.parse( Instagram.tag_recent_media( tag ).to_json )
+		response = []
 
+		tagArr = tag.split
+
+		return [] if tagArr.length > 2
+
+		tagArr.each {
+			|oneTag|
+
+			if response.empty?
+				response = JSON.parse( Instagram.tag_recent_media( oneTag, { count: 50 } ).to_json )
+			else
+				response = response & JSON.parse( Instagram.tag_recent_media( oneTag, { count: 50 } ).to_json )
+			end
+
+		}
+
+		response
+ 		
 	end
 
-	def self.idSearch( user_id )
+	# User id and tag search
+	# Example "1422420615 cat"
+	def self.id_tag_Search( tag )
 
-		JSON.parse( Instagram.user_recent_media( user_id ).to_json )
+		response = []
+
+		tagArr = tag.split
+
+		if is_number?( tagArr[0] )
+
+			result = JSON.parse( Instagram.user_recent_media( tagArr[0], { count: 50 } ).to_json )
+
+			result.each {
+				|x|
+
+				response << x if x["tags"].include?( tagArr[1] )
+			}
+
+
+		end 
+
+		response
 
 	end
 
@@ -89,9 +60,7 @@ class MyPhoto < ActiveRecord::Base
 		if str.include? ","
 			arr = str.split(",")
 
-			if arr.length != 2
-				return false
-			end
+			return false if arr.length != 2
 
 			check = true
 
@@ -116,50 +85,73 @@ class MyPhoto < ActiveRecord::Base
 										     :api_version => '20140701' )
 	end
 
-	def self.coordSearch( coords )
 
-		ll = coords.split(",")
+	#Coordinates search with tags or user_id
+	#search_id - true -> coords with user_id search
+	#search_id - false -> coords with tag search
+	#Example "34.071,-118.445 summer"
+	def self.coord_user_Search( tag, search_id )
+
+		tagArr = tag.split
+
+		ll = tagArr[0].split( "," )
 
 		response = JSON.parse( Instagram.location_search( ll[0], ll[1] ).to_json )
 
-		arr = Array.new
+		photourl = []
 
 		response.each {
 			|element|
 
-			arr.push( element["id"] )
+			arr = JSON.parse( Instagram.location_recent_media( element["id"], { count: 50 } ).to_json )
+
+			arr.each {
+				|e|
+				
+				photourl << e if search_id and e.is_a?( Hash ) and e["user"]["id"] == tagArr[1]
+
+				photourl << e if !search_id and e.is_a?( Hash ) and e["tags"].include? tagArr[1]
+			}	
 		}
 
-		photourl = Array.new
+		puts photourl
 
-		arr.each {
-			|element|
-
-			photourl.push( JSON.parse( Instagram.location_recent_media( element ).to_json ) )
-		}
-		
-		arrConf = Array.new
-
-		photourl.each {
-			|x|
-
-			if x[0].is_a?( Hash )
-				arrConf.push( x[0] )
-			end
-		}
-
-		arrConf
+		photourl
 	end
 
-	def self.checkinSearch( tag )
+	#Search by coords with user id and coords with tag
+	#search_id - true -> coords with user_id search
+	#search_id - false -> coords with tag search
+	#Example "53c3800c498e0abe55f520f0 food"
+	def self.check_in_tag_Search( tag, search_id )
 
 		initializeClient
 
-		response = JSON.parse( @client.checkin( tag ).to_json )
+		tagArr = tag.split
 
-		coords = response["venue"]["location"]["lat"].to_s + "," + response["venue"]["location"]["lng"].to_s
+		response = JSON.parse( @client.checkin( tagArr[0] ).to_json )
 
-		coordSearch( coords )
+		coords = response["venue"]["location"]["lat"].to_s + "," + response["venue"]["location"]["lng"].to_s + " " + tagArr[1]
+
+		coord_user_Search( coords, search_id )
+
+	end
+
+	#Search by coords with user id and with tag
+	#Example "53c3800c498e0abe55f520f0 food 210579418"
+	def self.check_in_tag_user_id_Search( tag )
+
+		initializeClient
+
+		tagArr = tag.split
+
+		response = JSON.parse( @client.checkin( tagArr[0] ).to_json )				
+
+		coords1 = response["venue"]["location"]["lat"].to_s + "," + response["venue"]["location"]["lng"].to_s + " " + tagArr[1]
+
+		coords2 = response["venue"]["location"]["lat"].to_s + "," + response["venue"]["location"]["lng"].to_s + " " + tagArr[2]
+
+		response = coord_user_Search( coords1, false ) & coord_user_Search( coords2, true )
 
 	end
 
